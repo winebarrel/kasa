aa2266dd547bb6f4c08fc8a68b33e705a60651cb
winebarrel <sugawara@winebarrel.jp>
2022-01-10 16:46:14 +0900

    Add TestMovePostBodyString

---
+++ esa/model/move_post_test.go
@@ -0,0 +1,24 @@
+package model_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/winebarrel/kasa/esa/model"
+)
+
+func TestMovePostBodyString(t *testing.T) {
+	assert := assert.New(t)
+	assert.Nil(nil)
+
+	tests := map[*model.MovePostBody]string{
+		{Name: "foo", Category: "bar/zoo"}:  "bar/zoo/foo",
+		{Name: "foo", Category: ""}:         "foo",
+		{Name: "", Category: "bar/zoo"}:     "bar/zoo",
+		{Name: "foo", Category: "/bar/zoo"}: "bar/zoo/foo",
+	}
+
+	for data, expected := range tests {
+		assert.Equal(data.String(), expected)
+	}
+}

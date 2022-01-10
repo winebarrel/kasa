63416b4004bbe1f965f2b32460a323170970eeed
winebarrel <sugawara@winebarrel.jp>
2022-01-10 18:18:06 +0900

    Remove deafult message

--- subcmd/post.go
+++ subcmd/post.go
@@ -5,7 +5,6 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
-	"time"
 
 	"github.com/winebarrel/kasa"
 	"github.com/winebarrel/kasa/esa/model"
@@ -60,19 +59,13 @@ func (cmd *PostCmd) Run(ctx *kasa.Context) error {
 		bodyMd = []byte{}
 	}
 
-	msg := cmd.Message
-
-	if msg == "" {
-		msg = "Posted on " + time.Now().Format(time.RFC3339)
-	}
-
 	newPost := &model.NewPostBody{
 		Name:     cmd.Name,
 		BodyMd:   string(bodyMd),
 		Tags:     cmd.Tags,
 		Category: cmd.Category,
 		Wip:      cmd.Wip,
-		Message:  msg,
+		Message:  cmd.Message,
 	}
 
 	url, err := ctx.Driver.Post(newPost, cmd.PostNum)

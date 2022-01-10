2d0a6cc25b79230761ac36100de8398eeb5e9a48
winebarrel <sugawara@winebarrel.jp>
2022-01-10 16:19:04 +0900

    Add mvcat cmd test

---
+++ subcmd/mvcat_test.go
@@ -0,0 +1,37 @@
+package subcmd_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/winebarrel/kasa"
+	"github.com/winebarrel/kasa/subcmd"
+)
+
+func TestMvcat(t *testing.T) {
+	assert := assert.New(t)
+	assert.Nil(nil)
+
+	mvcat := &subcmd.MvcatCmd{
+		From: "foo/bar",
+		To:   "bar/baz",
+	}
+
+	driver := NewMockDriver(t)
+	printer := &MockPrinterImpl{}
+
+	driver.MockMoveCategory = func(from string, to string) error {
+		assert.Equal("foo/bar", from)
+		assert.Equal("bar/baz", to)
+
+		return nil
+	}
+
+	err := mvcat.Run(&kasa.Context{
+		Driver: driver,
+		Fmt:    printer,
+	})
+
+	assert.NoError(err)
+	assert.Empty(printer.String())
+}

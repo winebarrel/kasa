d107a0e4899a4b2d25b3518df701640986e79ca3
winebarrel <sugawara@winebarrel.jp>
2022-01-11 11:20:59 +0900

    feat: Add install-completions command

--- cmd/kasa/main.go
+++ cmd/kasa/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"os"
+
 	"github.com/alecthomas/kong"
+	"github.com/posener/complete"
+	"github.com/willabides/kongplete"
 	"github.com/winebarrel/kasa"
 	"github.com/winebarrel/kasa/esa"
 	"github.com/winebarrel/kasa/subcmd"
@@ -10,29 +14,37 @@ import (
 var version string
 
 var cli struct {
-	Version kong.VersionFlag
-	Team    string           `required:"" env:"ESA_TEAM" help:"esa team"`
-	Token   string           `required:"" env:"ESA_TOKEN" help:"esa access token"`
-	Debug   bool             `help:"Debug flag."`
-	Cat     subcmd.CatCmd    `cmd:"" help:"Print post."`
-	Cp      subcmd.CpCmd     `cmd:"" help:"Copy posts."`
-	Info    subcmd.InfoCmd   `cmd:"" help:"Show post info."`
-	Ls      subcmd.LsCmd     `cmd:"" help:"List posts."`
-	Mv      subcmd.MvCmd     `cmd:"" help:"Move posts."`
-	Mvcat   subcmd.MvcatCmd  `cmd:"" help:"Move category."`
-	Mvs     subcmd.MvsCmd    `cmd:"" help:"Move posts from search results."`
-	Post    subcmd.PostCmd   `cmd:"" help:"New/Update post."`
-	Rm      subcmd.RmCmd     `cmd:"" help:"Delete posts."`
-	Rmi     subcmd.RmiCmd    `cmd:"" help:"Delete post by number."`
-	Rms     subcmd.RmsCmd    `cmd:"" help:"Delete posts from search results."`
-	Search  subcmd.SearchCmd `cmd:"" help:"Search posts."`
-	Tag     subcmd.TagCmd    `cmd:"" help:"Tagging posts."`
+	Version            kong.VersionFlag
+	Team               string                       `required:"" env:"ESA_TEAM" help:"esa team"`
+	Token              string                       `required:"" env:"ESA_TOKEN" help:"esa access token"`
+	Debug              bool                         `help:"Debug flag."`
+	Cat                subcmd.CatCmd                `cmd:"" help:"Print post."`
+	Cp                 subcmd.CpCmd                 `cmd:"" help:"Copy posts."`
+	Info               subcmd.InfoCmd               `cmd:"" help:"Show post info."`
+	Ls                 subcmd.LsCmd                 `cmd:"" help:"List posts."`
+	Mv                 subcmd.MvCmd                 `cmd:"" help:"Move posts."`
+	Mvcat              subcmd.MvcatCmd              `cmd:"" help:"Move category."`
+	Mvs                subcmd.MvsCmd                `cmd:"" help:"Move posts from search results."`
+	Post               subcmd.PostCmd               `cmd:"" help:"New/Update post."`
+	Rm                 subcmd.RmCmd                 `cmd:"" help:"Delete posts."`
+	Rmi                subcmd.RmiCmd                `cmd:"" help:"Delete post by number."`
+	Rms                subcmd.RmsCmd                `cmd:"" help:"Delete posts from search results."`
+	Search             subcmd.SearchCmd             `cmd:"" help:"Search posts."`
+	Tag                subcmd.TagCmd                `cmd:"" help:"Tagging posts."`
+	InstallCompletions kongplete.InstallCompletions `cmd:"" help:"Install shell completions"`
 }
 
 func main() {
-	ctx := kong.Parse(&cli, kong.Vars{"version": version})
+	parser := kong.Must(&cli, kong.Vars{"version": version})
 
-	err := ctx.Run(&kasa.Context{
+	kongplete.Complete(parser,
+		kongplete.WithPredictor("file", complete.PredictFiles("*")),
+	)
+
+	ctx, err := parser.Parse(os.Args[1:])
+	parser.FatalIfErrorf(err)
+
+	err = ctx.Run(&kasa.Context{
 		Team:   cli.Team,
 		Driver: esa.NewDriver(cli.Team, cli.Token, cli.Debug),
 		Fmt:    &kasa.PrinterImpl{},

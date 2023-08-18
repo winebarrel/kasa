2e46f93d352ec4af14c07c576c6a28a348c98263
winebarrel <genki.sugawara@kanmu.co.jp>
2023-08-18 10:21:18 +0900

    Support import without name

--- subcmd/import_test.go
+++ subcmd/import_test.go
@@ -45,3 +45,37 @@ func TestImport(t *testing.T) {
 	assert.NoError(err)
 	assert.Equal("https://docs.esa.io/posts/1\n", printer.String())
 }
+
+func TestImport_WithoutName(t *testing.T) {
+	assert := assert.New(t)
+	bodyFile, _ := os.CreateTemp("", "bodyMd")
+	bodyFile.WriteString("bodyMd") //nolint:errcheck
+	defer os.Remove(bodyFile.Name())
+
+	imp := &subcmd.ImportCmd{
+		File: bodyFile.Name(),
+		Path: "foo/bar/",
+	}
+
+	driver := NewMockDriver(t)
+	printer := &MockPrinterImpl{}
+
+	driver.MockPost = func(newPostBody *model.NewPostBody, postNum int, notice bool) (string, error) {
+		assert.Regexp(`^bodyMd`, newPostBody.Name)
+		assert.Equal("foo/bar/", newPostBody.Category)
+		assert.Equal("bodyMd", newPostBody.BodyMd)
+
+		assert.Equal(0, postNum)
+		assert.False(notice)
+
+		return "https://docs.esa.io/posts/1", nil
+	}
+
+	err := imp.Run(&kasa.Context{
+		Driver: driver,
+		Fmt:    printer,
+	})
+
+	assert.NoError(err)
+	assert.Equal("https://docs.esa.io/posts/1\n", printer.String())
+}
